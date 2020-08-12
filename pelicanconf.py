0f9a871e334252462057b81132f412711bbbc5b2
Mangala Gowri <mangalagb@gmail.com>
2020-08-12 18:40:29 +0200

    Gowri's personal blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,24 +4,18 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Gowri'
 SITENAME = "Gowri's Blog"
-SITEURL = ""
 THEME = 'themes/nest'
-SITESUBTITLE = "Gowri's Personal Blog"
-
+SITESUBTITLE = "A Personal blog of coding and travels"
+SITEURL = ''
 
 PATH = 'content'
 TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = 'en'
 
 # Static files
-STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/logo.svg']
-EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-    'extra/logo.svg': {'path': 'logo.svg'}
-}
+STATIC_PATHS = ['images']
 
-
+##############################################################################
 # Theme related settings
 
 # Add canonical link element to top page header and all article/author/category/tag page header
@@ -30,6 +24,7 @@ NEST_REL_CANONICAL_LINK = True
 MENUITEMS = [('Homepage', '/'),('Categories','/categories.html')]
 
 NEST_HEADER_IMAGES = 'hubble.jpg'
+NEST_HEADER_LOGO = '/images/g.jpg'
 
 # index.html
 NEST_INDEX_HEAD_TITLE = "Homepage"
@@ -37,6 +32,24 @@ NEST_INDEX_HEADER_TITLE = "Gowri's Blog"
 NEST_INDEX_HEADER_SUBTITLE = "Data Scientist & Space buff"
 NEST_INDEX_CONTENT_TITLE = "Recent Posts"
 
+DISQUS_SITENAME = 'molivier'
+
+##############################################################################
+
+# Plugins
+# Path to the folder containing the plugins
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['ipynb.markup']
+MARKUP = ('md', 'ipynb')
+IGNORE_FILES = [".ipynb_checkpoints"]  
+#IPYNB_USE_METACELL = True
+IPYNB_SKIP_CSS = True
+IPYNB_FIX_CSS = True
+
+from pelican_jupyter import markup as nb_markup
+PLUGINS = [nb_markup]
+
+##############################################################################
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
