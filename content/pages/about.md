c380702e097b3686b6b7a88b208e6011eb9a9721
Mangala Gowri <mangalagb@gmail.com>
2020-08-12 19:20:34 +0200

    About me page

---
+++ content/pages/about.md
@@ -0,0 +1,15 @@
+Title: About me
+Date: 2020-08-12 00:00
+Category: AboutMe
+Slug: about-me
+Summary: Info about me
+Tags: aboutme
+Authors: Gowri
+
+
+<p align="center">
+  <img src="../images/gowri.jpeg" />
+</p>
+
+
+Hello, I am Gowri.
--- pelicanconf.py
+++ pelicanconf.py
@@ -58,17 +58,9 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-
 # Social widget
-SOCIAL = (('github', 'https://github.com/mangalagb'),
-          ('linkedin','https://www.linkedin.com/in/mangalagowri/'))
+SOCIAL = (('Github', 'https://github.com/mangalagb'),
+          ('Linkedin','https://www.linkedin.com/in/mangalagowri/'))
 
 
 DEFAULT_PAGINATION = 10
